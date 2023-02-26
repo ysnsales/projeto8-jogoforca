f16c00b444ee66a89a2e7ba6252b391584f8181f
Yasmin <ysnsaless@gmail.com>
2023-02-26 16:48:28 -0300

    data-test's colocados

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -3,11 +3,11 @@ export default function Jogo(props){
     
     return (
         <div className="Jogo">
-        <img src={props.imagensErro} alt={props.imagensErro} />
+        <img data-test="game-image" src={props.imagensErro} alt={props.imagensErro} />
 
         <div>
-        <button onClick={props.EscolherPalavra}>Escolher Palavra</button>
-        <p style={{color:props.cor}}>{(props.palavraMostrada) }</p>
+        <button data-test="choose-word" onClick={props.EscolherPalavra}>Escolher Palavra</button>
+        <p data-test="word" style={{color:props.cor}}>{(props.palavraMostrada) }</p>
         </div>
         </div>
     )
