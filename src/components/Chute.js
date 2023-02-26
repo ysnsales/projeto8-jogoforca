27ad85ee63319cce413212986dc4d1f0b675d6d3
Yasmin <ysnsaless@gmail.com>
2023-02-26 19:51:50 -0300

    ajuste : data-test bônus

--- src/components/Chute.js
+++ src/components/Chute.js
@@ -2,8 +2,8 @@ export default function Chute(props){
     return (
         <div className="Chute">
         <p>Já sei a palavra</p>
-        <input disabled={props.disabled} className="input" onChange={chute => props.Chutar(chute)} ></input>
-        <button onClick={props.EnviarChute}>Chutar</button>
+        <input data-test="guess-input" disabled={props.disabled} className="input" onChange={chute => props.Chutar(chute)} ></input>
+        <button data-test="guess-button" onClick={props.EnviarChute}>Chutar</button>
         </div>
     )
 
